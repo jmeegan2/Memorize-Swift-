77b361175a62902aa38427ab7fe9ba4195ba3dc3
jmeegan2 <jmeegan8@gmail.com>
2023-03-02 21:47:10 -0500

    initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by James Meegan on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
