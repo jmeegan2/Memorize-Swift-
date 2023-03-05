e56e5b1760713b8d9bcfb337e19bedac47233ef3
jmeegan2 <jmeegan8@gmail.com>
2023-03-04 21:55:38 -0500

    update working through first vid

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -5,19 +5,50 @@
 //  Created by James Meegan on 3/2/23.
 //
 
-import SwiftUI
+import SwiftUI  //made by apple and ships with all apple devices
 
+//keywords in magenta like struct, ContentView is just the name, ": View" struct will behave like a view
+//View has alot of functionality but there are certain responsibilities when you are a view
 struct ContentView: View {
+    //after declaring its a view u have to specify its a view again with a var
+    // a function
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+            
+            return ZStack(content: {
+                    RoundedRectangle(cornerRadius: 20.0)
+                    .stroke(lineWidth: 3)
+                    .padding(.horizontal)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+                
+                Text("Hello, CS193p").foregroundColor(Color.orange).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            })
+            // another view that behaves like a text
+            //padding exist in all structs that behave like a view
+            //padding is a view modifier
+        
+//
         }
-        .padding()
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
