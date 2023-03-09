476419a9c24b251b69a4eb9f189bada72b11116f
jmeegan2 <jmeegan8@gmail.com>
2023-03-08 22:08:52 -0500

    added button to add cards and remove and logic

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,17 +10,49 @@ import SwiftUI  //made by apple and ships with all apple devices
 //keywords in magenta like struct, ContentView is just the name, ": View" struct will behave like a view
 //View has alot of functionality but there are certain responsibilities when you are a view
 struct ContentView: View {
-    var emojis = ["🚂","🚀","🚁","🚜","🚗","🏎","🛻","🦽","🚕","🚓","🚚","🦼","🚙","🚑","🦼","🚌","🚒","🚜","🛴","🚔","🚠","🚟","🚍","🚘"]
+   var emojis = ["🚂","🚀","🚁","🚜","🚗","🏎","🛻","🦽","🚕","🚓","🚚","🦼","🚙","🚑","🦼","🚌","🚒","🚜","🛴","🚔","🚠","🚟","🚍","🚘"]
     //after declaring its a view u have to specify its a view again with a var
     // a function
+    @State var emojiCount = 4
     var body: some View {
-        HStack {
-            ForEach(emojis, id: \.self) { emoji in
-                CardView(content: emoji)
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
             }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(.red)
+    }
+    var remove: some View {
+        Button(action: {
+            //this is what gives the button the ability to remove cards
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        }, label: {
+            //In order to the add the image I went to SF for ios devs and found the images
+            Image(systemName: "minus.circle")
+        })
+    }
+    var add: some View {
+        Button(action: {
+            //this is what gives the button the ability to add cards
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
     }
 }
 
@@ -54,7 +86,7 @@ struct CardView :View {
 
 // stopped at 14:02 https://www.youtube.com/watch?v=3lahkdHEhW8
 // stopped at 46:25
-
+//stopped at 1:00:33
 
 
 
